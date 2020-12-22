95a507ad1280c1c99b2ce0351ed97d205c790650
greyhoundforty <ryantiffany@fastmail.com>
2020-12-22 15:17:25 -0600

    Removed cert for the time being
    Signed-off-by: greyhoundforty <ryantiffany@fastmail.com>

--- dns/main.tf
+++ dns/main.tf
@@ -1,12 +1,12 @@
-resource "ibm_certificate_manager_order" "gitlab_cert" {
-  certificate_manager_instance_id = var.certificate_manager_instance
-  name                            = "gitlab-certificate"
-  description                     = "Certificate for gitlab vpc instance"
-  domains                         = ["gitlab.cde-lab.cloud"]
-  rotate_keys                     = false
-  domain_validation_method        = "dns-01"
-  dns_provider_instance_crn       = var.cis_instance
-}
+# resource "ibm_certificate_manager_order" "gitlab_cert" {
+#   certificate_manager_instance_id = var.certificate_manager_instance
+#   name                            = "gitlab-certificate"
+#   description                     = "Certificate for gitlab vpc instance"
+#   domains                         = ["gitlab.cde-lab.cloud"]
+#   rotate_keys                     = false
+#   domain_validation_method        = "dns-01"
+#   dns_provider_instance_crn       = var.cis_instance
+# }
 
 
 
