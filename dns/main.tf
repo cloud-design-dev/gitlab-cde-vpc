0dab8e310bbf8075ea3bd685885b15b2f697af8b
greyhoundforty <ryantiffany@fastmail.com>
2020-12-22 12:33:10 -0600

    Removing cert order for now
    Signed-off-by: greyhoundforty <ryantiffany@fastmail.com>

--- dns/main.tf
+++ dns/main.tf
@@ -1,12 +1,12 @@
-resource "ibm_certificate_manager_order" "gitlab_cert" {
-  certificate_manager_instance_id = data.ibm_resource_instance.certificate_manager.id
-  name                            = "gitlab-certificate"
-  description                     = "Certificate for gitlab vpc instance"
-  domains                         = ["gitlab.cde-lab.cloud"]
-  rotate_keys                     = false
-  domain_validation_method        = "dns-01"
-  dns_provider_instance_crn       = data.ibm_cis.cis_instance.id
-}
+# resource "ibm_certificate_manager_order" "gitlab_cert" {
+#   certificate_manager_instance_id = data.ibm_resource_instance.certificate_manager.id
+#   name                            = "gitlab-certificate"
+#   description                     = "Certificate for gitlab vpc instance"
+#   domains                         = ["gitlab.cde-lab.cloud"]
+#   rotate_keys                     = false
+#   domain_validation_method        = "dns-01"
+#   dns_provider_instance_crn       = data.ibm_cis.cis_instance.id
+# }
 
 
 
